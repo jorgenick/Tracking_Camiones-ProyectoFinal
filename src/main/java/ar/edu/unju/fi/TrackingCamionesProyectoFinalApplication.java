c9dc8ad795629d9a77d5fa502fc577aa4a6a2e5d
jorge <jorge@JORGEE>
2020-06-12 12:34:16 -0300

    Inicializacion de proyecto Responsable: Jorge Tolaba

---
+++ src/main/java/ar/edu/unju/fi/TrackingCamionesProyectoFinalApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingCamionesProyectoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingCamionesProyectoFinalApplication.class, args);
+	}
+
+}
